909b72f010a13c4b0f607486e3440ea7af2bf585
Cipher <shieldfiofficial@gmail.com>
2026-03-11 13:55:15 +0200

    Update PolicyNFTInteraction.js

--- src/PolicyNFTInteraction.js
+++ src/PolicyNFTInteraction.js
@@ -1,1 +1,38 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import PolicyNFTAbi from "../contracts/PolicyNFT.json"; 
 
+const POLICY_NFT_ADDRESS = "0xYourPolicyNFTAddress";
+
+export default function PolicyNFTInteraction({ account }) {
+  const [txStatus, setTxStatus] = useState("");
+
+  async function mintPolicy() {
+    if (!window.ethereum) return alert("Install MetaMask");
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const policyNFT = new ethers.Contract(POLICY_NFT_ADDRESS, PolicyNFTAbi, signer);
+
+    try {
+      const tx = await policyNFT.mintPolicy(
+        account,
+        ethers.utils.parseEther("1"),
+        30 * 24 * 60 * 60
+      );
+      setTxStatus("Transaction sent...");
+      await tx.wait();
+      setTxStatus("Policy minted!");
+    } catch (err) {
+      console.error(err);
+      setTxStatus("Error minting policy");
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={mintPolicy}>Mint Policy NFT</button>
+      <p>{txStatus}</p>
+    </div>
+  );
+}
