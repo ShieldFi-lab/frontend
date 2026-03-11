aef23cf3f3f1c8e69a7b30a2d88c11c4bc440342
Cipher <shieldfiofficial@gmail.com>
2026-03-11 13:57:28 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
+import { useState } from "react";
+import WalletConnection from "./components/WalletConnection";
+import PolicyNFTInteraction from "./components/PolicyNFTInteraction";
+import CoveragePoolInteraction from "./components/CoveragePoolInteraction";
 
+function App() {
+  const [account, setAccount] = useState(null);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>ShieldFi MVP</h1>
+      <WalletConnection setAccount={setAccount} />
+      {account && (
+        <>
+          <PolicyNFTInteraction account={account} />
+          <CoveragePoolInteraction account={account} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
